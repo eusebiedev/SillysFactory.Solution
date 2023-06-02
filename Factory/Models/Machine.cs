fbe184710b0d730a5428bd7ab5adf9472022fa5e
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-02 07:53:56 -0700

    Bugfix: remove engineer from machine model

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -8,9 +8,6 @@ namespace Factory.Models
     public int MachineId { get; set; }
     [Required(ErrorMessage = "The machine's name can't be empty!")]
     public string Name { get; set; }
-    [Range(1, int.MaxValue, ErrorMessage = "You must add your machine to an enginer. Have you created an engineer yet?")]
-    public int EngineerId { get; set; }
-    public Engineer Engineer { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
 }
